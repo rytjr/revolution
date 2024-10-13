f825ab108c8935add124c9e4ef5eab7775ffa4f2
rytjr <kks007159@naver.com>
2024-10-13 19:15:33 +0900

    circle

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.print(num*num2);
+    }
+}
